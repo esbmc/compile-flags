9628d3777181319e6bc52bbca780e73bb6bddba5
Yiannis Charalambous <yiannis128@hotmail.com>
2025-10-14 22:52:41 +0100

    tests: added dummy

---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present Yiannis Charalambous <yiannis128@hotmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0
+
+"""Dummy test file until we get some actual tests."""
+
+
+def test_dummy() -> None:
+    assert 1
